a8bf0c9a57acf801e14aaba42b2bb20bf4a0e835
zupzep <zupzep.dev@gmail.com>
2025-10-23 13:46:28 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,14 @@
 window.addEventListener('DOMContentLoaded', event => {
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        if (localStorage.getItem('zd|sidebar-toggle') === 'true') {
-            document.body.classList.toggle('zd-sidenav-toggled');
-        }
+        // if (localStorage.getItem('zd|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('zd-sidenav-toggled');
+        // }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('zd-sidenav-toggled');
             localStorage.setItem('zd|sidebar-toggle', document.body.classList.contains('zd-sidenav-toggled'));
         });
     }
+
 });
